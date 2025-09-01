884525cb8636db88b0e2a7208c313b4ab15a947c
Akhil Sai Ram <117731933+akkhil97@users.noreply.github.com>
2025-09-01 15:55:00 +0530

    deployment link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   standalone: true,
 
   /////    SPRIGBOOT PORT URL /////
-  apiBaseUrl: 'http://ec2-3-109-201-223.ap-south-1.compute.amazonaws.com:8080',
+  apiBaseUrl: 'http://ec2-3-110-176-73.ap-south-1.compute.amazonaws.com:8080',
 };
