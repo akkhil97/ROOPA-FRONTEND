6189c6b26346a37616e82d8d41f166038cbda921
Akhil Sai Ram <117731933+akkhil97@users.noreply.github.com>
2025-09-02 16:34:30 +0530

    sample url test 2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   standalone: true,
 
   /////    SPRIGBOOT PORT URL /////
-  apiBaseUrl: 'https://ec2-13-203-201-70.ap-south-1.compute.amazonaws.com:8080',
+  apiUrl: 'https://roopaeducationalsociety.com/api',
 };
