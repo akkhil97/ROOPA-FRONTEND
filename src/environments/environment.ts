c54575e7729107ca13da0a3858c2311eeabb097d
Akhil Sai Ram <117731933+akkhil97@users.noreply.github.com>
2025-09-02 18:15:03 +0530

    ssl domain link added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   standalone: true,
 
   /////    SPRIGBOOT PORT URL ////
-  apiBaseUrl: 'http://ec2-13-203-201-70.ap-south-1.compute.amazonaws.com:8080',
+  apiBaseUrl: 'https://api.roopaeducationalsociety.com',
 };
